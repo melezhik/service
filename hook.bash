bf805dcff8631ec53169de6b892e9a40a7e3c638
Alexey Melezhik <melezhik@gmail.com>
2016-08-19 13:42:32 +0300

    handle unknown OS

--- hook.bash
+++ hook.bash
@@ -1,4 +1,3 @@
-set_stdout OK
 
 service=$(config service)
 
@@ -6,9 +5,14 @@ shopt -s nocasematch;
 
 if [[ $os =~ "debian" ]]; then
   run_story $(config action) os debian service $service
+  set_stdout OK
 elif [[ "$os" =~ "ubuntu" ]]; then
   run_story $(config action) os debian service $service
+  set_stdout OK
 elif [[ "$os" =~ "centos" ]]; then
   run_story $(config action) os $os service $service
+  set_stdout OK
+else
+  set_stdout "unknown os: $os"
 fi
 
