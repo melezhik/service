364fbec83b65239115c49001997880c99b6dbf1b
Alexey Melezhik <melezhik@gmail.com>
2016-08-21 22:04:57 +0300

    using meta story

--- hook.bash
+++ hook.bash
@@ -5,14 +5,12 @@ shopt -s nocasematch;
 
 if [[ $os =~ "debian" ]]; then
   run_story $(config action) os debian service $service
-  set_stdout OK
 elif [[ "$os" =~ "ubuntu" ]]; then
   run_story $(config action) os debian service $service
-  set_stdout OK
 elif [[ "$os" =~ "centos" ]]; then
   run_story $(config action) os $os service $service
-  set_stdout OK
 else
-  set_stdout "unknown os: $os"
+  echo "unknown os: $os"
+  exit 1
 fi
 
